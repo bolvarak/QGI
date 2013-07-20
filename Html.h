7c0ac00d49dd918f64173246a0188227e55026d0
Travis Brown <tmbrown6@gmail.com>
2013-07-19 23:56:02 -0400

    Adding HTML tags constants

--- Html.h
+++ Html.h
@@ -289,163 +289,389 @@ namespace QGI
 			/// Constants ////////////////////////////////////////////////////
 			/////////////////////////////////////////////////////////////////
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the button input
+			 * @brief QGI::Html::InputButton
+			 * @var QString
+			 */
 			static QString InputButton;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the checkbox input
+			 * @brief QGI::Html::InputCheckbox
+			 * @var QString
+			 */
 			static QString InputCheckbox;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the file input
+			 * @brief QGI::Html::InputFile
+			 * @var QString
+			 */
 			static QString InputFile;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the hidden input
+			 * @brief QGI::Html::InputHidden
+			 * @var QString
+			 */
 			static QString InputHidden;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the image input
+			 * @brief QGI::Html::InputImage
+			 * @var QString
+			 */
 			static QString InputImage;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the password input
+			 * @brief QGI::Html::InputPassword
+			 * @var QString
+			 */
 			static QString InputPassword;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the radio input
+			 * @brief QGI::Html::InputRadio
+			 * @var QString
+			 */
 			static QString InputRadio;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the reset input
+			 * @brief QGI::Html::InputReset
+			 * @var QString
+			 */
 			static QString InputReset;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the submit input
+			 * @brief QGI::Html::InputSubmit
+			 * @var QString
+			 */
 			static QString InputSubmit;
 
-
+			/**
+			 * @paragraph This constant defines the text for the type attribute for the text input
+			 * @brief QGI::Html::InputText
+			 * @var QString
+			 */
 			static QString InputText;
 
-
+			/**
+			 * @paragraph This constant defines the text for the a tag
+			 * @brief QGI::Html::TagAnchor
+			 * @var QString
+			 */
 			static QString TagAnchor;
 
-
+			/**
+			 * @paragraph This constant defines the text for the body tag
+			 * @brief QGI::Html::TagBody
+			 * @var QString
+			 */
 			static QString TagBody;
 
-
+			/**
+			 * @paragraph This constant defines the text for the b tag
+			 * @brief QGI::Html::TagBold
+			 * @var QString
+			 */
 			static QString TagBold;
 
-
+			/**
+			 * @paragraph This constant defines the text for the button tag
+			 * @brief QGI::Html::TagButton
+			 * @var QString
+			 */
 			static QString TagButton;
 
-
+			/**
+			 * @paragraph This constant defines the text for the caption tag
+			 * @brief QGI::Html::TagCaption
+			 * @var QString
+			 */
 			static QString TagCaption;
 
-
+			/**
+			 * @paragraph This constant defines the text for the dd tag
+			 * @brief QGI::Html::TagDefinitionDescription
+			 * @var QString
+			 */
 			static QString TagDefinitionDescription;
 
-
+			/**
+			 * @paragraph This constant defines the text for the dl tag
+			 * @brief QGI::Html::TagDefinitionList
+			 * @var QString
+			 */
 			static QString TagDefinitionList;
 
-
+			/**
+			 * @paragraph This constant defines the text for the dt tag
+			 * @brief QGI::Html::TagDefinitionTerm
+			 * @var QString
+			 */
 			static QString TagDefinitionTerm;
 
-
+			/**
+			 * @paragraph This constant defines the text for the div tag
+			 * @brief QGI::Html::TagDivider
+			 * @var QString
+			 */
 			static QString TagDivider;
 
-
+			/**
+			 * @paragraph This constant defines the text for the fieldset tag
+			 * @brief QGI::Html::TagFieldset
+			 * @var QString
+			 */
 			static QString TagFieldset;
 
-
+			/**
+			 * @paragraph This constant defines the text for the form tag
+			 * @brief QGI::Html::TagForm
+			 * @var QString
+			 */
 			static QString TagForm;
 
-
+			/**
+			 * @paragraph This constant defines the text for the head tag
+			 * @brief QGI::Html::TagHead
+			 * @var QString
+			 */
 			static QString TagHead;
 
-
+			/**
+			 * @paragraph This constant defines the text for the h1 tag
+			 * @brief QGI::Html::TagHeader1
+			 * @var QString
+			 */
 			static QString TagHeader1;
 
-
+			/**
+			 * @paragraph This constant defines the text for the h2 tag
+			 * @brief QGI::Html::TagHeader2
+			 * @var QString
+			 */
 			static QString TagHeader2;
 
-
+			/**
+			 * @paragraph This constant defines the text for the h3 tag
+			 * @brief QGI::Html::TagHeader3
+			 * @var QString
+			 */
 			static QString TagHeader3;
 
-
+			/**
+			 * @paragraph This constant defines the text for the h4 tag
+			 * @brief QGI::Html::TagHeader4
+			 * @var QString
+			 */
 			static QString TagHeader4;
 
-
+			/**
+			 * @paragraph This constant defines the text for the h5 tag
+			 * @brief QGI::Html::TagHeader5
+			 * @var QString
+			 */
 			static QString TagHeader5;
 
+			/**
+			 * @paragraph This constant defines the text for the hr tag
+			 * @brief QGI::Html::TagHorizontalRule
+			 * @var QString
+			 */
+			static QString TagHorizontalRule;
 
+			/**
+			 * @paragraph This constant defines the text for the html tag
+			 * @brief QGI::Html::TagHtml
+			 * @var QString
+			 */
 			static QString TagHtml;
 
-
+			/**
+			 * @paragraph This constant defines the text for the input tag
+			 * @brief QGI::Html::TagInput
+			 * @var QString
+			 */
 			static QString TagInput;
 
-
+			/**
+			 * @paragraph This constant defines the text for the i tag
+			 * @brief QGI::Html::TagItalics
+			 * @var QString
+			 */
 			static QString TagItalics;
 
-
+			/**
+			 * @paragraph This constant defines the text for the label tag
+			 * @brief QGI::Html::TagLabel
+			 * @var QString
+			 */
 			static QString TagLabel;
 
-
+			/**
+			 * @paragraph This constant defines the text for the legend tag
+			 * @brief QGI::Html::TagLegend
+			 * @var QString
+			 */
 			static QString TagLegend;
 
-
+			/**
+			 * @paragraph This constant defines the text for the link tag
+			 * @brief QGI::Html::TagLink
+			 * @var QString
+			 */
 			static QString TagLink;
 
-
+			/**
+			 * @paragraph This constant defines the text for the li tag
+			 * @brief QGI::Html::TagListItem
+			 * @var QString
+			 */
 			static QString TagListItem;
 
-
+			/**
+			 * @paragraph This constant defines the text for the meta tag
+			 * @brief QGI::Html::TagMeta
+			 * @var QString
+			 */
 			static QString TagMeta;
 
+			/**
+			 * @paragraph This constant defines the text for the option tag
+			 * @brief QGI::Html::TagOption
+			 * @var QString
+			 */
+			static QString TagOption;
 
+			/**
+			 * @paragraph This constant defines the text for the ol tag
+			 * @brief QGI::Html::TagOrderedList
+			 * @var QString
+			 */
 			static QString TagOrderedList;
 
-
+			/**
+			 * @paragraph This constant defines the text for the p tag
+			 * @brief QGI::Html::TagParagraph
+			 * @var QString
+			 */
 			static QString TagParagraph;
 
-
+			/**
+			 * @paragraph This constant defines the text for the script tag
+			 * @brief QGI::Html::TagScript
+			 * @var QString
+			 */
 			static QString TagScript;
 
-
+			/**
+			 * @paragraph This constant defines the text for the select tag
+			 * @brief QGI::Html::TagSelect
+			 * @var QString
+			 */
 			static QString TagSelect;
 
-
+			/**
+			 * @paragraph This constant defines the text for the span tag
+			 * @brief QGI::Html::TagSpan
+			 * @var QString
+			 */
 			static QString TagSpan;
 
-
+			/**
+			 * @paragraph This constant defines the text for the strong tag
+			 * @brief QGI::Html::TagStrong
+			 * @var QString
+			 */
 			static QString TagStrong;
 
-
+			/**
+			 * @paragraph This constant defines the text for the style tag
+			 * @brief QGI::Html::TagStyle
+			 * @var QString
+			 */
 			static QString TagStyle;
 
-
+			/**
+			 * @paragraph This constant defines the text for the table tag
+			 * @brief QGI::Html::TagTable
+			 * @var QString
+			 */
 			static QString TagTable;
 
-
+			/**
+			 * @paragraph This constant defines the text for the tbody tag
+			 * @brief QGI::Html::TagTableBody
+			 * @var QString
+			 */
 			static QString TableBody;
 
-
+			/**
+			 * @paragraph This constant defines the text for the td tag
+			 * @brief QGI::Html::TagTableData
+			 * @var QString
+			 */
 			static QString TagTableData;
 
-
+			/**
+			 * @paragraph This constant defines the text for the th tag
+			 * @brief QGI::Html::TagTableDataHeader
+			 * @var QString
+			 */
 			static QString TagTableDataHeader;
 
-
+			/**
+			 * @paragraph This constant defines the text for the tfoot tag
+			 * @brief QGI::Html::TagTableFooter
+			 * @var QString
+			 */
 			static QString TableFooter;
 
-
+			/**
+			 * @paragraph This constant defines the text for the thead tag
+			 * @brief QGI::Html::TagTableHeader
+			 * @var QString
+			 */
 			static QString TagTableHeader;
 
-
+			/**
+			 * @paragraph This constant defines the text for the tr tag
+			 * @brief QGI::Html::TagTableRow
+			 * @var QString
+			 */
 			static QString TagTableRow;
 
-
+			/**
+			 * @paragraph This constant defines the text for the textarea tag
+			 * @brief QGI::Html::TagTextarea
+			 * @var QString
+			 */
 			static QString TagTextarea;
 
-
+			/**
+			 * @paragraph This constant defines the text for the title tag
+			 * @brief QGI::Html::TagTitle
+			 * @var QString
+			 */
 			static QString TagTitle;
 
-
+			/**
+			 * @paragraph This constant defines the text for the u tag
+			 * @brief QGI::Html::TagUnderline
+			 * @var QString
+			 */
 			static QString TagUnderline;
 
-
+			/**
+			 * @paragraph This constant defines the text for the ul tag
+			 * @brief QGI::Html::TagUnorderedList
+			 * @var QString
+			 */
 			static QString TagUnorderedList;
 
 			///////////////////////////////////////////////////////////////////
